83ea8f19ccf38aa3321acd0725020cb5506f35a2
rbonifacio <rbonifacio123@gmail.com>
2016-03-07 12:50:38 -0500

    export de results of the PP to the same file used as input

--- java/antenna-pp/src/br/unb/cic/hephaestus/preprocessor/antenna/Main.java
+++ java/antenna-pp/src/br/unb/cic/hephaestus/preprocessor/antenna/Main.java
@@ -29,8 +29,27 @@ public class Main {
 				for(String f: files) {
 					System.out.println("[Hephaestus PreProcessor] preprocessing file: " + f);
 					InputStream in   = new FileInputStream(new File(f));
-					OutputStream out = new FileOutputStream(new File(f + ".pp"));
+					OutputStream out = new FileOutputStream(new File(f + ".tmp"));
 					pp.preprocess(in, out, "UTF-8");	
+					in.close();
+					out.close();
+					
+					File tmpFile = new File(f + ".tmp");
+				    boolean success = tmpFile.renameTo(new File(f));
+					
+				    System.out.print("- renaming temp file");
+				    if(success) {
+				    	System.out.println("... [ok] ");
+				    }
+				    else {
+				    	System.out.println("...[nok]");
+				    }
+				    
+				    //tmpFile = new File(f + ".tmp");
+					//success = tmpFile.delete();
+					
+					//System.out.println(" - deleting temp file");
+					
 					//System.out.println("[Hephaestus PreProcessor] output: " + f);
 				}
 				
