430dd0fcda08ea2c89d05c8bd3d230f396980d12
Leomar Camargo <leomarcamargodesouza@gmail.com>
2018-02-27 15:59:01 -0300

    Corrigindo o método para remoção de linhas

--- java/antenna-pp/src/br/unb/cic/hephaestus/preprocessor/antenna/Main.java
+++ java/antenna-pp/src/br/unb/cic/hephaestus/preprocessor/antenna/Main.java
@@ -13,88 +13,84 @@ import java.nio.file.*;
 import java.nio.charset.*;
 import java.util.List;
 
-import antenna.preprocessor.PreprocessorException;
-import antenna.preprocessor.v3.PreprocessorBridge;
-
+import antenna.preprocessor.v2.PPException;
+import antenna.preprocessor.v2.Preprocessor;
 
 public class Main {
 
 	public static void main(String args[]) {
 		System.out.println("number of arguments: " + args.length);
-		if(args.length == 3 || (args[2].equals("--drop-lines") || args[2].equals("--comment-lines"))) {
+		if (args.length == 3 || (args[2].equals("--drop-lines") || args[2].equals("--comment-lines"))) {
 			try {
 				List<String> files = Files.readAllLines(Paths.get(args[0]), Charset.forName("UTF-8"));
-				List<String> tags  = Files.readAllLines(Paths.get(args[1]), Charset.forName("UTF-8"));
-				
-				PreprocessorBridge pp = new PreprocessorBridge(null);
-				
-				for(String tag: tags) {
-					pp.addSymbols(tag);
+				List<String> tags = Files.readAllLines(Paths.get(args[1]), Charset.forName("UTF-8"));
+
+				Preprocessor pp = new Preprocessor(null, null);
+
+				for (String tag : tags) {
+					pp.addDefines(tag);
 				}
-				
-				for(String f: files) {
+
+				for (String f : files) {
 					runPreprocessor(pp, f);
-				    if(args[2].equals("--drop-lines")) {
-				    	dropLines(args, f);
-				    }
+					if (args[2].equals("--drop-lines")) {
+						dropLines(args, f);
+					}
 				}
-			}
-			catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				System.out.println("error: " + e.getMessage());
 			}
-		}
-		else {
+		} else {
 			showErrorMessage();
 		}
 	}
 
-	private static void runPreprocessor(PreprocessorBridge pp, String f)
-			throws FileNotFoundException, IOException, PreprocessorException {
+	private static void runPreprocessor(Preprocessor pp, String f)
+			throws FileNotFoundException, IOException, PPException {
 		System.out.println("[Hephaestus PreProcessor] preprocessing file: " + f);
-		InputStream in   = new FileInputStream(new File(f));
+		InputStream in = new FileInputStream(new File(f));
 		OutputStream out = new FileOutputStream(new File(f + ".tmp"));
-		pp.preprocess(in, out, "UTF-8");	
+		pp.preprocess(in, out, "UTF-8");
 		in.close();
 		out.close();
-		
+
 		File tmpFile = new File(f + ".tmp");
 		boolean success = tmpFile.renameTo(new File(f));
-		
+
 		System.out.print("- renaming temp file");
-		if(success) {
+		if (success) {
 			System.out.println("... [ok] ");
-		}
-		else {
+		} else {
 			System.out.println("...[nok]");
 		}
 	}
 
-	private static void dropLines(String[] args, String f)
-			throws IOException {
+	private static void dropLines(String args[], String f) throws IOException {
 		List<String> lines = Files.readAllLines(Paths.get(f), Charset.forName("UTF-8"));
 		PrintWriter pw = new PrintWriter(new FileWriter(f));
 		boolean empty = false;
-	
+
 		for (String string : lines) {
-			if(!string.trim().startsWith("//#")) {
-				if(string.trim().equals("") && !empty) {
+			if (!string.trim().startsWith("//#") && !string.trim().startsWith("//@")
+					&& !string.trim().startsWith("// #")) {
+				if (string.trim().equals("") && !empty) {
 					pw.println(string);
 					empty = true;
-				}
-				else if (!string.trim().equals("")) {
+				} else if (!string.trim().equals("")) {
 					pw.println(string);
 					empty = false;
 				}
 			}
 		}
-	
+
 		pw.flush();
 		pw.close();
 	}
-	
+
 	private static void showErrorMessage() {
 		System.out.println("Expecting three command line arguments. ");
-		System.out.println("USAGE: java - jar antenna-pp.jar <ppfiles_file> <tags_file> <--drop-lines | --comment-lines>");
+		System.out.println(
+				"USAGE: java - jar antenna-pp.jar <ppfiles_file> <tags_file> <--drop-lines | --comment-lines>");
 	}
 }
